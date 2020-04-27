43c0a5f68ee031438678187191c280e3667ad81e
Rohit prasad <rohitprasad25061999@gmail.com>
2020-04-27 17:04:46 +0530

    Update UniqueBST.py

--- Unique BST/UniqueBST.py
+++ Unique BST/UniqueBST.py
@@ -1,7 +1,7 @@
 #For n number of nodes there are following possibilities:-
 # 1)0 node in left subtree and n-1 nodes in right subtree
 # 2)2 node in left subtree and n-2 nodes in right subtree
-.........
+#.........
 # i)i node in left subtree and n-i nodes in right subtree
 #hence the solution for n nodes will be:-
 # l[0]*l[n-1]+l[1][n-2]+......+l[n-1][0]
